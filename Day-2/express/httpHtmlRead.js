5bdff00a2820c2b8aaf49160777683f9b6bdb3e4
Aryan Tewari <aryantewari308@gmail.com>
2025-08-09 10:36:25 +0530

    http module sending html

--- Day-2/express/httpHtmlRead.js
+++ Day-2/express/httpHtmlRead.js
@@ -1,11 +1,17 @@
 const http = require("http");
+const fs = require("fs");
 
 const PORT = 3000;
 
 const server = http.createServer((request, response) => {
   if (request.url === "/home") {
-    //response.write()
-    response.end("hey this is a server response");
+    fs.readFile("./index.html", (err, data) => {
+      if (err) {
+        alert("the page has not been found");
+      } else {
+        response.end(data.toString());
+      }
+    });
   }
 });
 
